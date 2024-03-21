bb983f1774fc25dcf7aba12c21c4f407c4af9f67
devlrakib <devrakib5@gmail.com>
2024-03-21 16:56:30 +0600

    first commit

---
+++ max.js
@@ -0,0 +1,39 @@
+// ?==========================================> Max-Two Javascript Code <=======================================
+const jim = 84;
+const dela = 75;
+if (jim > dela){
+    console.log('Jim Will Get The Cake');
+}
+else{
+    console.log('Dela Will Get The Cake');
+}
+const jim1 = 69;
+const dela1 = 97;
+if (jim1 > dela1){
+    console.log('Jim Will Get The Cake');
+}
+else{
+    console.log('Dela Will Get The Cake');
+}
+
+// ?==========================================> Max-Three Javascript Code <=======================================
+const jim3 = 84;
+const dela3 = 99;
+const chinku = 77;
+if (jim3 > dela3 && jim3 > chinku){
+    console.log('Jim Will Get The Price');
+}
+else if (dela3 > jim3 && dela3 > chinku){
+    console.log('Dela Will Get The Price');
+}
+else{
+    console.log('Chinku Will Get The Price');
+}
+
+// ?==========================================> Max Short Formula Javascript Code <=======================================
+const shortMax = Math.max(84, 99, 77);
+console.log('This is javascript Short Max Formula',shortMax);
+
+// ?==========================================> Min Short Formula Javascript Code <=======================================
+const shortMin = Math.min (12, 14, 9, 10, 3)
+console.log('This is javascript Short Min Formula',shortMin);
